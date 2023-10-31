2c3900ed179be1a1075e38f45657b27da4c47693
JS <kanjury@naver.com>
2023-11-01 00:10:57 +0900

    test

--- src/test1030/Test4.java
+++ src/test1030/Test4.java
@@ -20,51 +20,47 @@ import java.util.Scanner;
 
 public class Test4 {
 	public static void main(String[] args) {
-		int numarr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		String answer = ""; // 시스템이 저장한 4개의 숫자
-		for (int i = 0; i <= 1000; i++) {
-			int f = (int) (Math.random() * 10); //0~9 사이의 임의의 수
-			int t = (int) (Math.random() * 10); //0~9 사이의 임의의 수
-			//swap 알고리즘
-			int temp = numarr[f];
-			numarr[f] = numarr[t];
-			numarr[t] = temp;
+		Scanner sc = new Scanner(System.in);
+		int[] arr = {0,1,2,3,4,5,6,7,8,9};
+		String sysNum = "";
+		for(int i=0; i<1000; i++) {
+			int t = (int)(Math.random() *10);
+			int f = (int)(Math.random() *10);
+			int tf = 0;
+			tf = arr[t];
+			arr[t] = arr[f];
+			arr[f] = tf;
 		}
-		for (int i = 0; i < 4; i++) {
-			answer += numarr[i];
-		}
-//		System.out.println(answer);  //정답 출력
-		Scanner scan = new Scanner(System.in);
-		int cnt = 0; //입력한 횟수 저장
-		while (true) {
-			System.out.println("서로다른 4자리 정수를 입력하세요");
-			String rcvNum = scan.nextLine();
-			cnt++;  //입력 횟수 1 증가
+		for(int i=0; i<4; i++) {
+			sysNum += arr[i];
+		}	
+		int cnt = 0;
+		while(true) {
 			int strike = 0;
 			int ball = 0;
-			//스트라익,볼 계산
-			for (int i = 0; i < answer.length(); i++) { //정답 글자
-				for (int j = 0; j < rcvNum.length(); j++) {  //입력한 숫자
-					if (answer.charAt(i) == rcvNum.charAt(j)) { //같은 값이 존재
-						if (i == j)	{
-							strike++;  //정답 위치와 입력한 숫자의 위치 같음
+			System.out.println("서로 다른 숫자 4자리를 입력해주세요");
+			String userNum = sc.nextLine();
+			cnt++;
+			for(int i=0; i<sysNum.length(); i++) {
+				for(int j=0; j<userNum.length(); j++) {
+					if(sysNum.charAt(i) == userNum.charAt(j)) {
+						if(i == j) {
+							strike++;
+						} else {
+							ball++;
 						}
-						else {
-							ball++;    //정답 위치와 입력한 숫자의 위치 다름
-						}
-						break;  //j for 구문 빠짐.
+						break;	
 					}
 				}
 			}
-			if (strike == 4) { //정답
-				System.out.println(rcvNum + "정답입니다.");
-				System.out.println(cnt + "번 만에 맞추셨습니다. 게임을 종료합니다.");
-				break; //while 반복문 빠짐
+			if(strike == 4) {
+				System.out.println("축하합니다! 정답입니다.");
+				System.out.println(cnt + "번 만에 맞추셨습니다.");
+				break;
 			} else {
-				System.out.println
-				  (rcvNum + ":" + strike + "스트라익, " + ball + "볼");
+				System.out.println(strike + "스트라이크 " + ball + "볼");
 			}
 		}
-		scan.close();
+		sc.close();
 	}
 }
