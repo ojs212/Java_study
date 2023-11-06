9b4491ba8e769c097f2d5a6a1dcacd819f4f9b7a
JS <kanjury@naver.com>
2023-11-06 22:47:04 +0900

    test

--- src/test1106/Test4.java
+++ src/test1106/Test4.java
@@ -8,34 +8,30 @@ interface LambdaInterface3 {
 	int method(int x, int y);
 }
 public class Test4 {
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 		LambdaInterface3 f = (x,y)-> x + y;
 		System.out.println("두수 (5,2)의 합:" + calc(5,2,f));//7
 		f = (x,y) -> x*y;
 		System.out.println("두수 (5,2)의 곱:" + calc(5,2,f));//10
 		int[] arr = {1,2,3,4,5,6,7,8,9,10};
-		int minIndex = 0;
-		int maxIndex = 0;
+		int minIndex = arr[0];
+		int maxIndex = arr[0];
 		for(int i=0; i<arr.length; i++) {
-			int tmp = 0;
-			for(int j=0; j<arr.length; j++) {
-				if(arr[i] > arr[j]) {
-					maxIndex = arr[i];
-				}
-				if (arr[i] > arr[j]) {
-					minIndex = arr[j];
-				}
+			if(maxIndex <= arr[i]) {
+				maxIndex = arr[i];
+			}
+			if(minIndex >= arr[i]) {
+				minIndex = arr[i];
 			}
 		}
-//		System.out.println("arr 배열의 최대값:" + calc(arr,f));//10
-//		System.out.println("arr 배열의 최소값:" + calc(arr,f));//1
-		System.out.println(minIndex + " " + maxIndex);
+		System.out.println("arr 배열의 최대값:" + calc(maxIndex,f));//10
+		System.out.println("arr 배열의 최소값:" + calc(minIndex,f));//1
 	}
-	
 	private static int calc(int x, int y, LambdaInterface3 f) {
 		return f.method(x, y);
 	}
-	private static void calc(int[] a, LambdaInterface3 f) {
-//		f.method(a);
+	private static int calc(int x, LambdaInterface3 f) {
+		return x;
 	}
+	
 }
