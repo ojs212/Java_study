99a0e3ad8a52d0a5032119fd2d6385c5130477fa
JS <kanjury@naver.com>
2023-11-09 13:06:09 +0900

    test

--- src/chap11/CalendarEx2.java
+++ src/chap11/CalendarEx2.java
@@ -8,7 +8,7 @@ public class CalendarEx2 {
 	public static void main(String[] args) {
 		Calendar day = Calendar.getInstance();
 		day.set(2023, (11 - 1),10); // 월 : 0~11
-		System.out.printf("날짜:%d-%d-%d\n", 
+		System.out.printf("날짜:%d-%d-%d\n",
 				day.get(Calendar.YEAR), day.get(Calendar.MONTH)+1,day.get(Calendar.DATE));
 		// 2023-12-31 ?요일 출력하기
 		String w = " 일월화수목금토"; // 요일 : 1(일) ~ 7(토)
