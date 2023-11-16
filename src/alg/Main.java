a9e07ce3d08d940990861ded197dd929e7cf5228
JS <kanjury@naver.com>
2023-11-16 17:52:50 +0900

    test

--- src/alg/Main.java
+++ src/alg/Main.java
@@ -5,21 +5,29 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String str = "ABC,DEF,GHI,JKL,MNO,PQRS,TUV,WXYZ";
-		String[] arr = str.split(",");
-		String s = sc.next();
+		String s = sc.next().trim();
+		String[] arr = s.split("");
 		int result = 0;
-		for(int i=0; i<15; i++) {
-			char c = 'A';
-			if(s.indexOf(c) >= 'A' && s.indexOf(c) <= 'C') {
+		for(int i=0; i<s.length(); i++) {
+			if("ABC".contains(arr[i])) {
 				result += 3;
-				c++;
-			} else {
-				c++;
-				break;
+			} else if("DEF".contains(arr[i])) {
+				result += 4;
+			} else if("GHI".contains(arr[i])) {
+				result += 5;
+			} else if("JKL".contains(arr[i])) {
+				result += 6;
+			} else if("MNO".contains(arr[i])) {
+				result += 7;
+			} else if("PQRS".contains(arr[i])) {
+				result += 8;
+			} else if("TUV".contains(arr[i])) {
+				result += 9;
+			} else if("WXYZ".contains(arr[i])) {
+				result += 10;
 			}
 		}
-		System.out.println(result + "" + arr);
+		System.out.println(result);
 		sc.close();
 	}
 }
