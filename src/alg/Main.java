6f64907b364cadd80d0eaaf98a8cc9fc649af6db
JS <kanjury@naver.com>
2023-11-20 22:07:19 +0900

    t

--- src/alg/Main.java
+++ src/alg/Main.java
@@ -5,11 +5,28 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		int m = sc.nextInt();
-		for(int i=0; i<m; i++) {
-			
+		int h = sc.nextInt();
+		int star = h * 2 - 1;
+		for(int i=0; i<h; i++) {
+			for(int j=0; j<star; j++) {
+				if(star/2+i >= j && star/2-i <= j) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
 		}
+		for(int i=h-2; i>=0; i--) {
+			for(int j=0; j<star; j++) {
+				if(star/2+i >= j && star/2-i <= j) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		sc.close();
 	}
 }
